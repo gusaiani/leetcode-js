7b70c3e88e3801b18b869973bc8e5f381ed29d54
Gustavo Saiani <gustavo@poe.ma>
2019-11-08 17:03:24 -0300

    Solve countAndSay

---
+++ README.md
@@ -0,0 +1,17 @@
+# Leetcode’s Challenges
+
+In this repo I work on Leetcode’s challenges in Javascript.
+
+## Dependencies
+
+`node`
+
+`npm` or `yarn`
+
+## Install
+
+`yarn install` or `npm install`
+
+## Test
+
+`yarn test` or `npm test`
---
+++ countAndSay.js
@@ -0,0 +1,35 @@
+function countAndSay(n) {
+  return iterate(n, 1, '1')
+}
+
+function iterate(n, currentCount, string) {
+  if (n === currentCount) return string
+
+  const newString = buildNewString(string)
+
+  const newCount = currentCount + 1
+
+  return iterate(n, newCount, newString)
+}
+
+function buildNewString(string) {
+  const arr = string.split('')
+
+  let currentDigit = arr[0]
+  let currentDigitCount = 1
+  let newString = ''
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === currentDigit) {
+      currentDigitCount = currentDigitCount + 1
+    } else {
+      newString = newString + currentDigitCount + currentDigit
+      currentDigit = arr[i]
+      currentDigitCount = 1
+    }
+  }
+
+  return newString + currentDigitCount + currentDigit
+}
+
+module.exports = countAndSay
