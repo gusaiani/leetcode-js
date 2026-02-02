e041c3cb1bf6343eff7ec3e63e4508b4922cd82d
Gustavo Saiani <gustavo@poe.ma>
2026-02-02 09:19:48 -0300

    Palindrome number

---
+++ palindromeNumber.js
@@ -0,0 +1,18 @@
+function palindromeNumber(number) {
+  const string = String(number);
+  const array = string.split("");
+
+  while (array.length > 0) {
+    if (array.length === 1) {
+      return true;
+    }
+    
+    if (array.shift() != array.pop()) {
+      return false;
+    } 
+  }
+  
+  return true;
+}
+
+module.exports = palindromeNumber;
---
+++ tests/palindromeNumber.test.js
@@ -0,0 +1,14 @@
+const palindromeNumber = require('../palindromeNumber');
+
+test('For x = 121', () => {
+  expect(palindromeNumber(121)).toBe(true);
+});
+
+test('For x = -121', () => {
+  expect(palindromeNumber(-121)).toBe(false);
+});
+
+test('For x = 10', () => {
+  expect(palindromeNumber(10)).toBe(false);
+});
+    
