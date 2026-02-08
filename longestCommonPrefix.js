2716c124979f0cf038027b3e463ceac3a5f930ea
Gustavo Saiani <gustavo@poe.ma>
2026-02-08 11:01:47 -0300

    Longest commont prefix

---
+++ longestCommonPrefix.js
@@ -0,0 +1,40 @@
+function longestCommonPrefix(strings) {
+  let longestPrefix = [];
+  let hasDifferentPrefix = false;
+  let index = 0;
+
+  while (!hasDifferentPrefix) {
+    let currentChar = "";
+    let charsAreTheSame = true;
+    
+    strings.forEach((string, stringIndex) => {
+      if (stringIndex === 0) {
+        const char = string.charAt(index);
+
+        if (char === "") {
+          charsAreTheSame = false;
+          hasDifferentPrefix = true;
+        } else {
+          currentChar = char;
+        }
+      
+      } else {
+        const char = string.charAt(index);
+        if (char !== currentChar) {
+          charsAreTheSame = false;
+          hasDifferentPrefix = true;
+        }
+      }
+    })
+
+    if (charsAreTheSame) {
+      longestPrefix.push(currentChar);
+    }
+
+    index++;
+  }
+
+  return longestPrefix.join("");
+}
+
+module.exports = longestCommonPrefix
