d3f458f877a6d7159b191de0796ce16ab185c09e
Gustavo Saiani <gustavo@poe.ma>
2026-02-08 20:58:08 -0300

    Length of Last Word

---
+++ lengthOfLastWord.js
@@ -0,0 +1,20 @@
+function lengthOfLastWord(string) {
+  const array = string.split("");
+  let haveFoundFirstValidChar = false;
+  let length = 0;
+
+  for (let index = array.length - 1; index >= 0; index--) {
+    const char = array[index];
+
+    if (char === " ") {
+      if (haveFoundFirstValidChar) { 
+        return length; 
+      }
+    } else {
+      haveFoundFirstValidChar = true;
+      length++;
+    }
+  }
+}
+
+module.exports = lengthOfLastWord
