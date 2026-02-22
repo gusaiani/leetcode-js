37000b463a22ca6529d690f64c13c9497dc0a9e2
Gustavo Saiani <gustavo@poe.ma>
2026-02-22 10:38:05 -0300

    House Robber (#58)
    * House Robber
    
    * Fix formatting

---
+++ houseRobber.js
@@ -0,0 +1,42 @@
+/**
+ * LeetCode 198. House Robber
+ * https://leetcode.com/problems/house-robber/description/
+ *
+ * You are a professional robber planning to rob houses along a street. Each
+ * house has a certain amount of money stashed. The only constraint stopping
+ * you from robbing each of them is that adjacent houses have security systems
+ * connected, and it will automatically contact the police if two adjacent
+ * houses were broken into on the same night.
+ *
+ * Given an integer array nums representing the amount of money of each house,
+ * return the maximum amount of money you can rob tonight without alerting the
+ * police.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function rob(houseValues) {
+  if (houseValues.length === 0) {
+    return 0;
+  }
+
+  let maximumProfitFromTwoHousesAgo = 0;
+
+  let maximumProfitFromOneHouseAgo = 0;
+
+  for (const currentHouseValue of houseValues) {
+    const currentTotalIfRobbed = currentHouseValue + maximumProfitFromTwoHousesAgo;
+
+    const bestProfitAtCurrentStep = Math.max(currentTotalIfRobbed, maximumProfitFromOneHouseAgo);
+
+    maximumProfitFromTwoHousesAgo = maximumProfitFromOneHouseAgo;
+
+    maximumProfitFromOneHouseAgo = bestProfitAtCurrentStep;
+  }
+
+  return maximumProfitFromOneHouseAgo;
+}
+
+module.exports = { rob };
