3c6229dadfb38896ff3b70ac7ef26fb3ea3f28f6
Gustavo Saiani <gustavo@poe.ma>
2026-07-02 11:41:26 -0300

    Height Checker (#1051)

---
+++ heightChecker.ts
@@ -0,0 +1,32 @@
+/**
+ * LeetCode 1051. Height Checker
+ * https://leetcode.com/problems/height-checker/
+ *
+ * A school is trying to take an annual photo of all the students. The
+ * students are asked to stand in a single file line in non-decreasing
+ * order by height. Let this ordering be represented by the integer array
+ * `expected` where `expected[i]` is the expected height of the ith
+ * student in line.
+ *
+ * You are given an integer array `heights` representing the current order
+ * that the students are standing in. Each `heights[i]` is the height of
+ * the ith student in line (0-indexed).
+ *
+ * Return the number of indices where `heights[i] != expected[i]`.
+ */
+
+function heightChecker(heights: number[]): number {
+  let result = 0;
+
+  const sorted = [...heights].sort((a, b) => a - b);
+
+  for (let i = 0; i < heights.length; i++) {
+    if (heights[i] !== sorted[i]) {
+      result++;
+    }
+  }
+
+  return result;
+}
+
+export { heightChecker };
